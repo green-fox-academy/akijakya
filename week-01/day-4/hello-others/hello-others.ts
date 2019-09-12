56fb1080a911eb99dfc4fe3543a8fda402e9fd51
Andras Jaky <jakyandras@gmail.com>
2019-09-12 13:55:38 +0200

    Finished with the hello-others excercise

---
+++ week-01/day-4/hello-others/hello-others.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+
+console.log('Hello, Tibi!');
+console.log('Hello, Dani!');
+console.log('Hello, Misi!');
