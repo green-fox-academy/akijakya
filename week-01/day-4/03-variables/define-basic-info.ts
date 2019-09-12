556706e3d5c3c72db30738e6a0160f51974ab3e0
Andras Jaky <jakyandras@gmail.com>
2019-09-12 16:53:25 +0200

    Finished with the define-basic-info excercise

---
+++ week-01/day-4/03-variables/define-basic-info.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+let myName: string = 'Andras Jaky';
+// Your age as a number
+let myAge: number = 29;
+// Your height in meters as a number
+let myHeight: number = 1.79;
+// Whether you are married or not as a boolean
+let amIMarried: boolean = true;
+
+console.log(myName + '\n' + myAge + '\n' + myHeight + '\n' + amIMarried);
