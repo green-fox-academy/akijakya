918d23d905ae7045a66e96c74381282bed7566df
Andras Jaky <jakyandras@gmail.com>
2019-09-13 12:29:54 +0200

    Created a solution for the draw-pyramids excercise

---
+++ week-01/day-5/05-loops/draw-pyramid.ts
@@ -0,0 +1,27 @@
+'use strict';
+export{}
+
+let lineCount: number = 4;
+
+// Write a program that draws a
+// pyramid like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as lineCount is
+
+let stars: string = '*';
+let spaceCount: number = 1;
+
+for (let i:number = 0; i < lineCount; i++){
+  let spaces: string = '';
+  for (let j:number = 0; j < (lineCount-spaceCount); j++){
+      spaces = spaces + ' ';
+  }
+  spaceCount++
+  console.log(spaces + stars + spaces);
+  stars = stars + '**';
+}
