843e09f2fd4a9ccb0ce6b3db4036cb683565209f
Andras Jaky <jakyandras@gmail.com>
2019-09-13 11:12:52 +0200

    Finished with the draw-triangle excercise

---
+++ week-01/day-5/05-loops/draw-triangle.ts
@@ -0,0 +1,19 @@
+'use strict';
+export{}
+
+let lineCount: number = 4;
+
+// Write a program that draws a triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+let stars: string = '';
+
+for (let i:number = 0; i < lineCount; i++){
+    stars = stars + '*';
+    console.log(stars);
+}
