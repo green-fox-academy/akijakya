9854a3bafcb78ba1b56280469a798d172cf548c0
Andras Jaky <jakyandras@gmail.com>
2019-09-16 13:57:16 +0200

    Created another solution with the forEach method

--- week-02/day-1/01-Arrays/append-a.ts
+++ week-02/day-1/01-Arrays/append-a.ts
@@ -2,6 +2,8 @@
 //    with the following content: `["koal", "pand", "zebr"]`
 // -  Add all elements an `"a"` at the end
 
+//doing it with a for loop
+console.log("With for loop:");
 let animals:string [] = ["koal", "pand", "zebr"];
 
 for(let i:number = 0; i < animals.length; i++){
@@ -9,3 +11,13 @@ for(let i:number = 0; i < animals.length; i++){
 }
 
 console.log(animals);
+
+//doing it with forEach
+console.log("With forEach method:");
+let animals2:string [] = ["koal", "pand", "zebr"];
+
+animals2.forEach (function(value, index){
+    animals2[index] += "a";
+});
+
+console.log(animals2);
