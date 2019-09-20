27732d5af1d576c15e1e28d6cb334b46997ca9e7
Andras Jaky <jakyandras@gmail.com>
2019-09-20 17:12:48 +0200

    Started building a loop to rotate and draw the hexagon

--- week-02/day-5/super-hexagon-ts.ts
+++ week-02/day-5/super-hexagon-ts.ts
@@ -3,22 +3,29 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-function hexagon (size:number) {
-    let coorX: number = 0;
-    let coorY: number = 0;
-    ctx.setTransform(1, 0, 0, 1, canvas.width/2, canvas.width/2);
-
+function hexagon (coorX:number, coorY:number, size:number) {
     ctx.beginPath();
     ctx.moveTo(coorX - size/2, coorY + size*Math.sqrt(3)/2);
     ctx.lineTo(coorX + size/2, coorY + size*Math.sqrt(3)/2);
-    ctx.lineTo(coorX + size + size/2, coorY);
+    ctx.lineTo(coorX + size, coorY);
     ctx.lineTo(coorX + size/2, coorY - size*Math.sqrt(3)/2);
     ctx.lineTo(coorX - size/2, coorY - size*Math.sqrt(3)/2);
-    ctx.lineTo(coorX - size - size/2, coorY);
+    ctx.lineTo(coorX - size, coorY);
     ctx.closePath();
     ctx.stroke();
 }
 
-hexagon(20);
+hexagon(canvas.width/2, canvas.height/2, 20);
 
+function hexaPattern (size:number) {
+    let coorX: number = canvas.width/2;
+    let coorY: number = canvas.height/2;
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    for (let i:number = 0; i < 6; i++) {
+        hexagon(coorX, coorY - size*Math.sqrt(3), size);
+        ctx.rotate(60 * Math.PI/180);
+    }
+}
 // rotate: ctx.rotate(angle * Math.PI/180);
+
+hexaPattern(20);
