39e28aa709057de7ac2f2f8f2fb8c681a51332d1
Andras Jaky <jakyandras@gmail.com>
2019-09-20 16:07:24 +0200

    Trying to draw a hexagon in the middle of the canvas

--- week-02/day-5/super-hexagon-ts.ts
+++ week-02/day-5/super-hexagon-ts.ts
@@ -3,3 +3,22 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
+function hexagon (size:number) {
+    let coorX: number = 0;
+    let coorY: number = 0;
+    ctx.setTransform(1, 0, 0, 1, canvas.width/2, canvas.width/2);
+
+    ctx.beginPath();
+    ctx.moveTo(coorX - size/2, coorY + size*Math.sqrt(3)/2);
+    ctx.lineTo(coorX + size/2, coorY + size*Math.sqrt(3)/2);
+    ctx.lineTo(coorX + size + size/2, coorY);
+    ctx.lineTo(coorX + size/2, coorY - size*Math.sqrt(3)/2);
+    ctx.lineTo(coorX - size/2, coorY - size*Math.sqrt(3)/2);
+    ctx.lineTo(coorX - size - size/2, coorY);
+    ctx.closePath();
+    ctx.stroke();
+}
+
+hexagon(20);
+
+// rotate: ctx.rotate(angle * Math.PI/180);
