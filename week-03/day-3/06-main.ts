3115391d63c920f5769593ca6a140fb67ee1803d
Andras Jaky <jakyandras@gmail.com>
2019-09-25 14:48:26 +0200

    Solved the Pokemon excercise

---
+++ week-03/day-3/06-main.ts
@@ -0,0 +1,31 @@
+import { Pokemon } from './06-pokemon'
+
+let pokemonOfAsh: Pokemon[] = initializePokemon();
+
+// Every pokemon has a name and a type.
+// Certain types are effective against others, e.g. water is effective against fire.
+
+// Ash has a few pokemon.
+// A wild pokemon appeared!
+
+let wildPokemon: Pokemon = new Pokemon('Oddish', 'leaf', 'water');
+
+// Which pokemon should Ash use?
+let pokemonToChoose:string; 
+pokemonOfAsh.forEach (function (e) {
+    if (e.isEffectiveAgainst(wildPokemon)) {
+        pokemonToChoose = e.name;
+    }
+});
+
+console.log('I choose you, ' + pokemonToChoose);
+
+function initializePokemon(): Pokemon[] {
+    return [
+        new Pokemon('Balbasaur', 'leaf', 'water'),
+        new Pokemon('Pikatchu', 'electric', 'water'),
+        new Pokemon('Charizard', 'fire', 'leaf'),
+        new Pokemon('Balbasaur', 'water', 'fire'),
+        new Pokemon('Kingler', 'water', 'fire')
+    ];
+}
