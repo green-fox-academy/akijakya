984cef8083497a81a3c22b009136c4bc85634917
Andras Jaky <jakyandras@gmail.com>
2019-09-26 12:31:26 +0200

    Added export to Sharpie class

--- week-03/day-3/04-sharpie.ts
+++ week-03/day-3/04-sharpie.ts
@@ -5,7 +5,7 @@
     // We can use() the sharpie objects
     // which decreases inkAmount
 
-class Sharpie {
+export class Sharpie {
     private _color:string;
     private _width:number;
     private _inkAmount:number;
