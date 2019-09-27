d70b5c3ed8ae7e520abc624906428d453325ae23
Andras Jaky <jakyandras@gmail.com>
2019-09-27 10:54:58 +0200

    Solved the Sum digits exercise

---
+++ week-03/day-5/02-sum-digit.ts
@@ -0,0 +1,14 @@
+// Given a non-negative integer n, return the sum of its digits recursively (without loops).
+
+// JavaScript, TypeScript
+// There is no integer type in JavaScript. To remove the rightmost digit you must divide (/) the number by 10 and find a way to get the the whole number portion of that number.
+
+function sumDigit (n:number):number {
+    if (n*10 <= 100) {
+        return n;
+    } else {
+        return n%10 + Math.floor(sumDigit(Math.floor(n/10)));
+    }
+}
+
+console.log(sumDigit(56789));
