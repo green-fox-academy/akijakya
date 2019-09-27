9e5d3f14e9c3cee71143bac816ad8937edcec409
Andras Jaky <jakyandras@gmail.com>
2019-09-27 14:53:32 +0200

    Made some minor mofifications which made more sense

--- week-03/day-5/02-sum-digit.ts
+++ week-03/day-5/02-sum-digit.ts
@@ -4,7 +4,7 @@
 // There is no integer type in JavaScript. To remove the rightmost digit you must divide (/) the number by 10 and find a way to get the the whole number portion of that number.
 
 function sumDigit (n:number):number {
-    if (n*10 <= 100) {
+    if (n < 10) {
         return n;
     } else {
         return n%10 + Math.floor(sumDigit(Math.floor(n/10)));
