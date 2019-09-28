7229f18e669d43aad147351337596de81de5d738
Andras Jaky <jakyandras@gmail.com>
2019-09-28 22:39:10 +0200

    Worked some more on the Koch line

--- week-03/day-5/koch-line-ts.ts
+++ week-03/day-5/koch-line-ts.ts
@@ -29,21 +29,26 @@ function kochLine2 (x:number, y:number, size:number, rotation:number) {
     ctx.beginPath();
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 1;
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0+size, 0);
-    ctx.lineTo(0+size*3/2, 0-size*Math.sqrt(3)/2);
-    ctx.lineTo(0+size*2, 0);
-    ctx.lineTo(0+size*3, 0);
+    ctx.moveTo(x, y);
+    ctx.lineTo(x+size, y);
+    ctx.lineTo(x+size*3/2, y-size*Math.sqrt(3)/2);
+    ctx.lineTo(x+size*2, y);
+    ctx.lineTo(x+size*3, y);
     ctx.stroke();
     ctx.closePath();
-    // if (size >= 120) {
-    //     ctx.fillStyle = 'white';
-    //     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    //     kochLine(x, y, size/3, 0);
-    //     kochLine(x+size*2, y, size/3, 0);
-    //     kochLine(x+size, y, size/3, 300);
-    // }
+    
+    if (size >= 50) {
+        
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        kochLine2(0, 0, size/3, 0);
+        kochLine2(size*2, 0, size/3, 0);
+        kochLine2(size, 0, size/3, 300);
+        kochLine2(size*1.5, 0-size*Math.sqrt(3)/2, size/3, 60);
+        ctx.resetTransform();
+    }
     
 }
 
-kochLine(0, canvas.height/2, canvas.width/3);
+//kochLine(0, canvas.height/2, canvas.width/3);
+kochLine2(0, canvas.height/2, canvas.width/3, 0);
