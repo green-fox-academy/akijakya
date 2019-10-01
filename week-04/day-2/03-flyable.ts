59785fcc6127704c62593eb3e7a0461d16a0fc01
Andras Jaky <jakyandras@gmail.com>
2019-10-01 14:58:10 +0200

    Finished with the Flyable exercise

---
+++ week-04/day-2/03-flyable.ts
@@ -0,0 +1,42 @@
+export interface Flyable {
+    land();
+    fly();
+    takeOff();
+}
+ export abstract class Vehicle {
+    protected _name:string;
+    protected _numberOfSeats:number;
+    protected _color:string;
+
+    constructor (numberOfSeats?:number, color?:string, name?:string) {
+        this._name = name;
+        if (numberOfSeats !== undefined) {
+            this._numberOfSeats = numberOfSeats;
+        } else {
+            this._numberOfSeats = 2;
+        }
+        if (color !== undefined) {
+            this._color = color;
+        } else {
+            this._color = 'white';
+        }
+    }
+} 
+
+class Helicopter extends Vehicle implements Flyable {
+    constructor (numberOfSeats?:number, color?:string, name?:string) {
+        super(numberOfSeats, color, 'Helicopter')
+    }
+
+    land():void {
+        console.log('The ' + this._color + ' ' + this._name + ' landed successfully!');
+    }
+
+    fly ():void {
+        console.log('The ' + this._color + ' ' + this._name + ' is currently flying.');
+    }
+
+    takeOff ():void {
+        console.log('The ' + this._color + ' ' + this._name + ' took of successfully!');
+    }
+}
