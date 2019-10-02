87d6bd6da5144089b60abdc68d7b357074c3b3da
Andras Jaky <jakyandras@gmail.com>
2019-10-02 17:20:02 +0200

    made fibonacci function shorter

--- week-04/day-3/05-fibonacci.ts
+++ week-04/day-3/05-fibonacci.ts
@@ -1,8 +1,6 @@
 export function fibonacciSequence (index:number):number {
     let startArray:number [] = [0, 1];
-    if (index < 3 && index > 0) {
-        return index - 1;
-    } else if (index > 2) {
+    if (index > 0) {
         for (let i:number = 0; i < index-2; i++) {
             startArray.push(startArray[i] + startArray[i+1]);
         }
