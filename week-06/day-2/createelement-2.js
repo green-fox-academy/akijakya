f2e54e749a8d6cbaf1d7bf164ca2b67555aab715
András Jáky <jakyandras@gmail.com>
2019-10-24 11:21:04 +0200

    started to work on createelement-2 exercise

---
+++ week-06/day-2/createelement-2.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const planetData = [
+    {
+      category: 'inhabited',
+      content: 'Foxes',
+      asteroid: true,
+    },
+    {
+      category: 'inhabited',
+      content: 'Whales and Rabbits',
+      asteroid: true,
+    },
+    {
+      category: 'uninhabited',
+      content: 'Baobabs and Roses',
+      asteroid: true,
+    },
+    {
+      category: 'inhabited',
+      content: 'Giant monsters',
+      asteroid: false,
+    },
+    {
+      category: 'inhabited',
+      content: 'Sheep',
+      asteroid: true,
+    },
+];
+
+let asteroids = document.getElementsByTagName("ul")[0];
+let king = asteroids.getElementsByTagName("li")[0];
+asteroids.removeChild(king);
+
