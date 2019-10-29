5f908bbcefb3560f1b5a050842b7a0833af961c5
András Jáky <jakyandras@gmail.com>
2019-10-29 12:47:10 +0100

    inserted the still thumnail of a gif into the html page

--- week-04/week-07/day-2/giphy-api/giphy.js
+++ week-04/week-07/day-2/giphy-api/giphy.js
@@ -5,17 +5,18 @@ getJoke();
 
 function getJoke() {
     let request = new XMLHttpRequest();
-    request.open('GET', 'http://api.icndb.com/jokes/random', true);
+    request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=Hf2DWQvwX3bGj8bNgJkm34o5XCDgkIgw&tag=&rating=G', true);
 
     // let loadingElement = document.querySelector('.loader');
     // let isLoading = false;
 
     request.onload = function(){
         // will called when all the content is loaded => readyState = 4
-        let jokeElement = document.getElementById('joke');
+        let gifElement = document.getElementById('GIF');
         let response = JSON.parse(this.response);
-        let joke = response.value.joke;
-        jokeElement.innerText = joke;
+        let stillGIFThumbnail = response.data.images.fixed_height_still.url;
+        gifElement.setAttribute ("src", stillGIFThumbnail);
+        // data.images.fixed_height_still.url
         // setTimeout(function () {
         //     loadingElement.classList.add('hide');
         // }, 100);
