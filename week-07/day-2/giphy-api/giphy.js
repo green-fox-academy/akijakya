a0d69de9d043aa86542d660cba0a7d5391823e54
András Jáky <jakyandras@gmail.com>
2019-10-29 14:07:03 +0100

    added onclick function so the thumnail starts to move

--- week-07/day-2/giphy-api/giphy.js
+++ week-07/day-2/giphy-api/giphy.js
@@ -2,15 +2,13 @@
 
 let gifList = document.getElementById("GIFs");
 
-getGIFs();
-// setTimeout(getJoke, 4000);
+for (let i = 0; i < 16; i++) {
+    getGIFs();
+}
 
 function getGIFs() {
     let request = new XMLHttpRequest();
     request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=Hf2DWQvwX3bGj8bNgJkm34o5XCDgkIgw&tag=&rating=G', true);
-
-    // let loadingElement = document.querySelector('.loader');
-    // let isLoading = false;
 
     request.onload = function(){
         // will called when all the content is loaded => readyState = 4
@@ -22,22 +20,9 @@ function getGIFs() {
         newLi.setAttribute ("class", "GIFbox"); 
         gifList.appendChild(newLi);
         newLi.appendChild(newGIF);
-        // setTimeout(function () {
-        //     loadingElement.classList.add('hide');
-        // }, 100);
-
+        newGIF.onclick = function () {
+            newGIF.src = response.data.images.fixed_width_small.url;
+        };
     };
-
-    // request.onreadystatechange = function(){
-    //     if (!isLoading) {
-    //         // remove the hide class so the loader will be enabled
-    //         loadingElement.classList.remove('hide');
-    //         isLoading = true;
-    //     }
-    //     console.log(this.readyState);
-    // };
-
     request.send();
-
-
 }
