22210ea7032a96b2e8414c654a2dfe825562b3ab
András Jáky <jakyandras@gmail.com>
2019-10-30 13:23:50 +0100

    created the base files for the exercise

---
+++ week-07/day-3/REST-backend/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const path = require('path');
+const express = require ('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('assets'));
+
+app.get('/', function(req, res) {   
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`The server is up and running on ${PORT}`);
+});
