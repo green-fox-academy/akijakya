4f90a642c5112e5a17318aeb287aad01343cb8fd
András Jáky <jakyandras@gmail.com>
2019-10-30 13:47:27 +0100

    finished with the Doubling exercise

--- week-07/day-3/REST-backend/app.js
+++ week-07/day-3/REST-backend/app.js
@@ -3,12 +3,35 @@
 const path = require('path');
 const express = require ('express');
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(express.static('assets'));
 
 app.get('/', function(req, res) {   
 	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/doubling', (req, res) => {
+    if(req.query.input !== undefined){
+        let input = req.query.input;
+        let result = input * 2;
+
+        let resultObj = {
+            "received": input,
+            "result": result
+        }
+
+        res.status(200);
+        res.setHeader("Content-type", "application/json");
+        res.send(JSON.stringify(resultObj));
+    } else {
+        const errMessage = {
+            error: "Please provide an input!"
+        }
+        res.status(400);
+        res.setHeader("Content-type", "application/json");
+        res.send(JSON.stringify(errMessage));
+    }
 });
 
 app.listen(PORT, () => {
