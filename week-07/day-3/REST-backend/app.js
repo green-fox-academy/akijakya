72c9b12b06f048cd6fb8697c79e5a9f2bf855d34
András Jáky <jakyandras@gmail.com>
2019-10-30 14:17:06 +0100

    finished with the Greeter, fixed the Doubling exercise

--- week-07/day-3/REST-backend/app.js
+++ week-07/day-3/REST-backend/app.js
@@ -13,24 +13,57 @@ app.get('/', function(req, res) {
 
 app.get('/doubling', (req, res) => {
     if(req.query.input !== undefined){
-        let input = req.query.input;
-        let result = input * 2;
 
         let resultObj = {
-            "received": input,
-            "result": result
+            "received": req.query.input,
+            "result": req.query.input * 2
         }
 
-        res.status(200);
-        res.setHeader("Content-type", "application/json");
-        res.send(JSON.stringify(resultObj));
+        // res.status(200);
+        // res.setHeader("Content-type", "application/json");
+        res.send(resultObj);
     } else {
-        const errMessage = {
-            error: "Please provide an input!"
+
+        let errMessage = {
+            "error": "Please provide an input!"
         }
-        res.status(400);
-        res.setHeader("Content-type", "application/json");
-        res.send(JSON.stringify(errMessage));
+        
+        // res.status(400);
+        // res.setHeader("Content-type", "application/json");
+        res.send(errMessage);
+    }
+});
+
+app.get('/greeter', (req, res) => {
+    if (req.query.name && req.query.title !== undefined) {
+        let greeting = {
+            welcome_message: `Oh, hi there ${req.query.name}, my dear ${req.query.title}!`
+        }
+
+        // res.status(200);
+        // res.setHeader("Content-type", "application/json");
+        res.send(greeting);
+    } else if (req.query.name && req.query.title === undefined) {
+        let errMessage = {
+            error: "Please provide a name and a title!"
+        }
+        // res.status(400);
+        // res.setHeader("Content-type", "application/json");
+        res.send(errMessage);
+    } else if (req.query.name === undefined) {
+        let errMessage = {
+            error: "Please provide a name!"
+        }
+        // res.status(400);
+        // res.setHeader("Content-type", "application/json");
+        res.send(errMessage);
+    } else if (req.query.title === undefined) {
+        let errMessage = {
+            error: "Please provide a title!"
+        }
+        // res.status(400);
+        // res.setHeader("Content-type", "application/json");
+        res.send(errMessage);
     }
 });
 
