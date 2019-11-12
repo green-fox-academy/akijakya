d56d60236b3fa47e85585f63bca89c2d6f3adaed
András Jáky <jakyandras@gmail.com>
2019-11-12 17:25:45 +0100

    added yondu endpoint and test

--- week-09/day-2/iamgroot/routes.js
+++ week-09/day-2/iamgroot/routes.js
@@ -17,4 +17,21 @@ app.get('/groot', (req, res) => {
   }
 });
 
+app.get('/yondu', (req, res) => {
+  if (req.query.distance !== undefined && req.query.time !== undefined) {
+    let speed = req.query.distance / req.query.time;
+    res.status(200);
+    res.json({
+      "distance": parseFloat(parseFloat(req.query.distance).toFixed(1)),
+      "time": parseFloat(parseFloat(req.query.time).toFixed(1)),
+      "speed": parseFloat(parseFloat(speed).toFixed(1))
+    });
+  } else {
+    res.status(404);
+    res.json({
+      "error": "Give distance and time!"
+    });
+  }
+});
+
 module.exports = app;
--- week-09/day-2/iamgroot/test/test-routes.js
+++ week-09/day-2/iamgroot/test/test-routes.js
@@ -28,3 +28,17 @@ test('groot endpoint with message', (t) => {
         t.end();
         });
 });
+
+test('yondu endpoint with positive integers', (t) => {
+    request(app)
+        .get('/yondu?distance=100.0&time=10.0')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function (err, res) {
+        t.error(err, 'No error');
+        t.equal(res.body.distance, 100.0, 'should be 100.0 as float');
+        t.equal(res.body.time, 10.0, 'should be 10.0 as float');
+        t.equal(res.body.speed, 10.0, 'should be 10.0 as float');
+        t.end();
+        });
+});
