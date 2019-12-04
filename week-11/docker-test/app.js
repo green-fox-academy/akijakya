c9af84ca04aee8110006bec7b6894fbd6da5baba
Andras Jaky <jakyandras@gmail.com>
2019-12-04 09:17:32 +0100

    added docker-test to the repo

---
+++ week-11/docker-test/app.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// requiring path
+const path = require('path');
+
+// requiring express
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// connection with the database
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password', 
+    database: 'dockertest',
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.log('Error connecting to database');
+      return;
+    }
+    console.log('Connection to database established');
+});
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/greetings', function(req, res) {
+    connection.query('SELECT * FROM greetings;', function(err, result) {
+        if (err) {
+            res.status(500).send('Database error');
+            return;
+        }
+        res.status(200);
+        res.setHeader("Content-type", "application/json");
+        res.send(result);
+    });
+});
+
+app.post('/newgreeting', function(req, res) {
+    console.log(req.body);
+    connection.query(`INSERT INTO greetings (greeting) VALUES (?);`, [req.body.greeting], function(err, result) {
+        if (err) {
+            res.status(500).send('Database error');
+            return;
+        }
+        res.sendFile(path.join(__dirname, 'index.html'));
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`The server is up and running on ${PORT}`);
+});
---
+++ week-11/docker-test/dockertest.sql
@@ -0,0 +1,51 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Linux (x86_64)
+--
+-- Host: localhost    Database: dockertest
+-- ------------------------------------------------------
+-- Server version	8.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `greetings`
+--
+
+DROP TABLE IF EXISTS `greetings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `greetings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `greeting` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `greetings`
+--
+
+LOCK TABLES `greetings` WRITE;
+/*!40000 ALTER TABLE `greetings` DISABLE KEYS */;
+INSERT INTO `greetings` VALUES (1,'hello'),(2,'ciao'),(3,'szia'),(4,'buongiorno'),(5,'welcome'),(6,'willkommen'),(7,'jónapot'),(8,'proba'),(9,'bábulubá'),(10,'proba2'),(11,'valami'),(12,'jncladncknd');
+/*!40000 ALTER TABLE `greetings` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-03 22:00:50
