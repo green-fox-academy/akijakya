defef716f879531304e1e51e272055eab80e4181
akijakya <jakyandras@gmail.com>
2019-12-05 14:15:53 +0100

    changed bad character coding

--- week-11/docker-test/dockertest.sql
+++ week-11/docker-test/dockertest.sql
@@ -26,7 +26,7 @@ CREATE TABLE `greetings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `greeting` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
