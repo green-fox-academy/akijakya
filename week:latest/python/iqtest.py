84bb2791b7e45f1d8005a10fb07ec2eccf245542
akijakya <jakyandras@gmail.com>
2020-01-29 09:50:03 +0100

    practice to solve a codewars kata

---
+++ week:latest/python/iqtest.py
@@ -0,0 +1,38 @@
+# def iq_test(numbers):
+#     numArray = map(int, numbers.split(' '))
+#     if (is_even(numArray)):
+#         for i, num in enumerate(numArray, start=1):
+#             if num % 2 == 1:
+#                 print(i)
+#     else:
+#         for i, num in enumerate(numArray, start=1):
+#             if num % 2 == 0:
+#                 print(i)
+            
+def iq_test(numbers):
+    numArray = map(int, numbers.split(' '))
+    for i, num in enumerate(numArray, start=1):
+        if num % 2 == 1:
+            print(i)
+            
+def is_even(numArray):
+    even = 0
+    uneven = 0
+    for x in numArray:
+        if x % 2 == 1:
+            uneven += 1
+        else:
+            even += 1
+    if even > uneven:
+        print(True)
+    else:
+        print(False)
+
+arr = "2 2 1 4 6"
+arr2 = "1 3 7 6 5"
+arr3 = arr.split(' ')
+arr4 = list(map(int, arr3))
+arr5 = list(map(int, arr.split(' ')))
+
+iq_test(arr)
+print(arr4)
