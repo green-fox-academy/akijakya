0f2267548ba729a4486deef19220d9d0f7b53ea4
akijakya <jakyandras@gmail.com>
2020-02-03 20:33:35 +0100

    added test

--- week:latest/python/timeconversion.py
+++ week:latest/python/timeconversion.py
@@ -1,3 +1,5 @@
+import unittest
+
 def timeConversion(s):
     hour = s[:2]
     if s[-2:] == "AM":
@@ -12,6 +14,14 @@ def timeConversion(s):
             return f"{int(hour) + 12}{s[2:-2]}"
 
 
+# class TestTime(unittest.TestCase):
+
+#     def test_AM(self):
+#         self.assertEqual(timeConversion("12:34:23AM"), "00:34:23")
+
 
 
 print(timeConversion("12:34:23AM"))
+
+# if __name__ == "__main__":
+#     unittest.main()
