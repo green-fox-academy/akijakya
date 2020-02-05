7a4d185b039b58e41b269b3b9df20658474a42d2
akijakya <jakyandras@gmail.com>
2020-02-05 11:30:59 +0100

    da ultimate solution

--- week:latest/python/leaderboard.py
+++ week:latest/python/leaderboard.py
@@ -1,35 +1,27 @@
+import copy
+
 def climbingLeaderboard(scores, alice):
     keys = list(dict.fromkeys(scores))
-
     result = []
-
-    # for x in alice:
-    #     if x < keys[-1]:
-    #         result.append((len(keys) + 1))
-    #     else:
-    #         for i, y in enumerate (keys, start=1):
-    #             if x >= y:
-    #                 result.append(i)
-    #                 break
-
     i = len(keys) - 1
-    for x in alice:
+            
+    def findRank(x, i):
         if x > keys[0]:
             result.append(1)
-        else:
-            if x < keys[i]:
-                result.append(i + 2)
-                break
-            elif x == keys[i]:
-                result.append(i + 1)
-            else
+        elif x < keys[i]:
+            result.append(i + 2)
+        elif x == keys[i]:
+            result.append(i + 1)
+        else: 
+            i -= 1
+            i = findRank(x, i)
+        return i
 
-
-            # for i, y in enumerate (keys, start=1):
-            #     if x < keys[-i]:
-            #         result.append(len(keys) + 2 - i)
-            #         break
+    for x in alice:
+        i = findRank(x, i)
 
     return result
+
+
 
 print(climbingLeaderboard([100, 100, 50, 40, 40, 20, 10], [5, 25, 50, 120]))
