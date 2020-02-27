aa2a3005f6b7306431d695dfa42c05e691fcefb5
akijakya <jakyandras@gmail.com>
2020-02-27 21:03:02 +0100

    deleted hackerrank stuff

--- week:latest/sql/divisible.sql
+++
@@ -1,9 +0,0 @@
-select *, 
-CASE
-    WHEN n%x=0 AND n%y=0 
-        THEN 'true'
-    ELSE 'false'
-END AS res
-from kata;
-
-SELECT id, n % x = 0 AND n % y = 0 AS res FROM kata
--- week:latest/sql/pelda.sql
+++
@@ -1,4 +0,0 @@
-SELECT employee.name, job.type
-FROM employee
-LEFT JOIN job.type
-ON employee.job_id = job.id;
